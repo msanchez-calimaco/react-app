42ba16e6add336870d5f35351d1a1b87ea43909a
Miguel Sanchez <miguel.sanchez@calimaco.com>
2023-05-09 17:11:28 +0200

    feat: eld

--- release.config.js
+++ release.config.js
@@ -1,9 +1,13 @@
 module.exports = {
-  branches: "main",
-  repositoryUrl: "https://github.com/msanchez-calimaco/react-app",
-  plugins: [
-    "@semantic-release/commit-analyzer",
-    "@semantic-release/release-notes-generator",
-    "@semantic-release/github",
-  ],
-};
+	branches: "master",
+	repositoryUrl: "https://github.com/alialaa/react-app",
+	plugins: [
+		"@semantic-release/commit-analyzer", "@semantic-release/release-notes-generator", 
+		["@semantic-release/github", {
+			assets: [
+				{ path: "build.zip", label: "Build" },
+				{ path: "coverage.zip", label: "Coverage" }
+			]
+		}]
+		]
+}
